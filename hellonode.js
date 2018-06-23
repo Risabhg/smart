aa13b01015ee820102629e6ea1897f9c1df2a190
Rishabh <rishabh1997@gmail.com>
2018-06-23 11:08:59 +0530

    modified

--- hellonode.js
+++ hellonode.js
@@ -3,13 +3,17 @@ const REST_PORT = (process.env.PORT || 5000);
 
 http.createServer(function (request, response) {
 
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/html'});
-   
+
    // Send the response body as "Hello World"
+   var x=5;
+   var z=x+4;
    response.end('<b>Hello World...</b>\n');
+    response.end('<b>Hey there...</b>\n');
+   response.end(z);
 }).listen(REST_PORT);
 
 // Console will print the message
